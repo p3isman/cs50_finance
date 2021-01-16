2920608a39b108b662ae0f98fb6de93021df364f
RealMagus <pedro_eisman@hotmail.com>
2021-01-16 17:53:39 +0100

    add register

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def after_request(response):
 # Database connection
 db = SQL('sqlite:///finance.db')
 
-
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
@@ -35,10 +34,6 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
 
 
 @app.route("/")
@@ -113,14 +108,42 @@ def logout():
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
 def quote():
-    """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        quote = request.form.get("quote")
+        response = lookup(quote)
+        return render_template("show_quote.html", name=response['name'], 
+            price=response['price'], symbol=response['symbol'])
+    
+    else: 
+        return render_template("get_quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
-    return apology("TODO")
+    if request.method == "POST":
+        username = request.form.get("username")
+        existing_usernames = db.execute("SELECT username FROM users")
+
+        # Username checking
+        if len(username) < 4:
+            return apology("Username must contain at least 4 characters!", 422)
+        for user in existing_usernames:
+            if username == user:
+                return apology("Username already exists!")
+
+        # Password checking
+        if request.form.get("password") != request.form.get("password2"):
+            return apology("Passwords don't match!", 422)
+        if len(request.form.get("password")) < 4:
+            return apology("Password must contain at least 4 characters", 422)
+        else:
+            password_hash = generate_password_hash(request.form.get("password"))
+            db.execute("INSERT INTO users (username, hash), VALUES (:username, :hash)", 
+                username=username, hash=password_hash)
+            return redirect("/")
+            
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
--- helpers.py
+++ helpers.py
@@ -40,7 +40,7 @@ def lookup(symbol):
 
     # Contact API
     try:
-        api_key = os.environ.get("API_KEY")
+        api_key = "pk_56ccf1b397cf4db288914f0fe2409182"
         response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
         response.raise_for_status()
     except requests.RequestException:
